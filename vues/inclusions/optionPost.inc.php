01f2005bb3047eb1b16e20ae24d5d39b259d99e1
Danny Alexander Villeda <dvilleda@dept-info.crosemont.quebec>
2019-12-18 17:12:06 -0500

    corrections mineures

--- vues/inclusions/optionPost.inc.php
+++ vues/inclusions/optionPost.inc.php
@@ -1,13 +1,10 @@
 <?php
 if($controleur->getActeur()=="visiteur"){
 	echo "<div id='optionPublication'>";
-		echo "<p> Vous devez vous connecter pour donner un like ou signaler</p>";
+		echo "<p> Vous devez vous connecter pour signaler une image</p>";
 	echo "</div>";
 }else{
 		echo "<div id='optionPublication'>";
-			echo "<form action='' method='post'>";
-			echo "<input class='optionsgauche' type='Submit' name='1' value='Like' />";
-				echo "</form>";
 			echo "<button class='optionsdroite' onclick='signaler()'>Signaler</button>";
 		echo "</div>";
 }
